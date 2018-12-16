5acf7aa96b136cfb7aabb0d346fbbc22655e60ab
Grandduchy <jchallenger1@hotmail.com>
2018-12-16 15:20:18 -0500

    Added files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <thread>
+int factorial(const int& i) {
+	int l = 1;
+	for (int p = i; p != 1; --p){
+		l *= p;
+	}
+	return l;
+}
+
+bool test() {
+	factorial(10) == 3628800 ? 0 : 1;
+}
+
+int main() {
+	bool b = test();
+	std::cout << std::boolalpha << b;
+	return b;
+}
